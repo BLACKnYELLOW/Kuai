359f8e7bd2db651d556c0b8ebacae92050826814
s017787 Student <s017787@students.lmsd.org>
2023-02-22 14:00:33 -0500

    Initial Commit

---
+++ Kuai/KuaiApp.swift
@@ -0,0 +1,17 @@
+//
+//  KuaiApp.swift
+//  Kuai
+//
+//  Created by Asher Polsky (student LM) on 2/22/23.
+//
+
+import SwiftUI
+
+@main
+struct KuaiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
