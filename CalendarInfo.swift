a4eb541486da69e6f73453f9de540d13047e815c
Zach Bader <117207219+s016605@users.noreply.github.com>
2023-03-08 12:31:39 -0500

    Add files via upload

---
+++ CalendarInfo.swift
@@ -0,0 +1,21 @@
+//
+//  CalendarInfo.swift
+//  Kuai
+//
+//  Created by Zachary Bader (student LM) on 3/8/23.
+//
+
+import Foundation
+
+class CalendarInfo {
+    var dailyDate: String
+    var weeklyDate: [String]
+    var monthlyDate: [String]
+    
+    
+    init (dailyDate: String = "9-7-22", weeklyDate: [String] = [], monthlyDate: [String] = []) {
+        self.dailyDate = dailyDate
+        self.weeklyDate = weeklyDate
+        self.monthlyDate = monthlyDate
+    }
+}
---
+++ CalendarView.swift
@@ -0,0 +1,29 @@
+//
+//  CalendarView.swift
+//  Kuai
+//
+//  Created by Asher Polsky (student LM) on 2/27/23.
+//
+
+import SwiftUI
+
+struct CalendarView: View {
+    
+    var body: some View {
+        ZStack {
+            VStack {
+                HStack {
+                    Text("Hello")
+                        .padding()
+                        .background(Color.blue)
+                }
+            }
+        }
+    }
+}
+
+struct CalendarView_Previews: PreviewProvider {
+    static var previews: some View {
+        CalendarView()
+    }
+}
